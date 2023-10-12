4ffc12e77781f9b1e2d60f7762226d09f1699271
CloudStone <cloudneo71@gmail.com>
2023-10-12 17:28:18 +0800

    Bug: Merging Failed

--- Controllers/RegisterController.cs
+++ Controllers/RegisterController.cs
@@ -1,18 +1,9 @@
-﻿using EvaluationSystem.Data;
-using EvaluationSystem.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace EvaluationSystem.Controllers
 {
     public class RegisterController : Controller
     {
-        private readonly AppDbContext dataDb;
-
-        public RegisterController(AppDbContext data)
-        {
-            dataDb = data;
-        }
-
         public IActionResult RegisterIndex()
         {
             return View();
@@ -21,14 +12,6 @@ namespace EvaluationSystem.Controllers
         public IActionResult RegisterStudent()
         {
             return View();
-        }
-
-        [HttpPost]
-        public IActionResult RegisterStudent(StudentTableModel student)
-        {
-            dataDb.Students.Add(student); 
-            dataDb.SaveChanges();
-            return RedirectToAction("RegisterIndex");
         }
 
         public IActionResult RegisterProfessor()
