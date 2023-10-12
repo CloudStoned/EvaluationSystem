bb40a6891f6f2f24566062498ddc19352c385b5d
CloudStone <cloudneo71@gmail.com>
2023-10-12 16:46:30 +0800

    Feature: Create Account

--- Controllers/RegisterController.cs
+++ Controllers/RegisterController.cs
@@ -1,9 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EvaluationSystem.Data;
+using EvaluationSystem.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EvaluationSystem.Controllers
 {
     public class RegisterController : Controller
     {
+        private readonly AppDbContext dataDb;
+
+        public RegisterController(AppDbContext data)
+        {
+            dataDb = data;
+        }
+
         public IActionResult RegisterIndex()
         {
             return View();
@@ -12,6 +21,14 @@ namespace EvaluationSystem.Controllers
         public IActionResult RegisterStudent()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult RegisterStudent(StudentTableModel student)
+        {
+            dataDb.Students.Add(student); 
+            dataDb.SaveChanges();
+            return RedirectToAction("RegisterIndex");
         }
 
         public IActionResult RegisterProfessor()
