62050b30df4501b57ab9d7da158272487e703c59
CloudStone <cloudneo71@gmail.com>
2023-10-12 14:40:02 +0800

    Created Models

---
+++ Models/ProfessorTableModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EvaluationSystem.Models
+{
+    public class ProfessorTableModel
+    {
+        [Key]
+        public int professorId { get; set; }
+        [Required]
+        public string professorFirstName { get; set; }
+        public string professorLastName { get; set; }
+        public DateTime dateCreated { get; set; } = DateTime.Now;
+    }
+}
---
+++ Models/StudentAnswersTable.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace EvaluationSystem.Models
+{
+    public class StudentAnswersTable
+    {
+        [Key]
+        public int answerId { get; set; }
+
+        public int studentNumber { get; set; }
+        public string lastName { get; set; }
+        public string course { get; set; }
+        public int yearLevel { get; set; }
+
+        [Required]
+        public string answerOne { get; set; }
+        [Required]
+        public string answerTwo { get; set; }
+        [Required]
+        public string answerThree { get; set; }
+        [Required]
+        public string answerFour { get; set; }
+        [Required]
+        public string answerFive { get; set; }
+
+        public int professorId { get; set; }
+
+        [ForeignKey("studentNumber")]
+        public StudentTableModel Student { get; set; }
+        [ForeignKey("professorId")]
+        public ProfessorTableModel Professor { get; set; }
+        public DateTime dateEvaluated { get; set; }
+    }
+}
